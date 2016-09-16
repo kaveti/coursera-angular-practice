13f567408c9a14b2f5d34721b0f4712f92bed151
kaveti <kavetiraviteja1992@gmail.com>
2016-09-16 12:56:41 +0530

    CSS-ADDED-TO-FIRST-ASSIGNMENT

--- app.js
+++ app.js
@@ -9,6 +9,7 @@
 
     function listFood($scope){
       $scope.message = "";
+      $scope.customStyle = {};
       $scope.checkListOfItems = function validateList() {
         if(typeof $scope.listOfFoodItems !== "undefined"){
             var items = $scope.listOfFoodItems.split(",");
@@ -21,23 +22,27 @@
             checkLength(items,filteredItems);
         }else {
               $scope.message = "Please enter data first";
-        }
+              $scope.customStyle.color = "red";
       }
+    };
 
-      function checkLength(items,filteredItems){
-        if(filteredItems.length > 3){
-          $scope.message = "Too much!";
-        }else{
-          $scope.message = "Enjoy!";
-        }
-        if(filteredItems.length < 3 && items.length>=3){
-          $scope.message = "Please enter data Not Empty comma separated items";
-        }
+    function checkLength(items,filteredItems){
+      if(filteredItems.length > 3){
+        $scope.message = "Too much!";
+        $scope.customStyle.color = "red";
+      }else{
+        $scope.message = "Enjoy!";
+        $scope.customStyle.color = "green";
       }
-
-      function isEmpty(str) {
-          return str.replace(/^\s+|\s+$/g, '').length == 0;
+      if(filteredItems.length < 3 && items.length>=3){
+        $scope.message = "Please enter data! Not Empty OR Space comma separated items";
+        $scope.customStyle.color = "red";
       }
     }
+
+    function isEmpty(str) {
+        return str.replace(/^\s+|\s+$/g, '').length === 0;
+    }
   }
+}
 )();
